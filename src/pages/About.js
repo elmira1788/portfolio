34b41cd5e1f273bd404374234e9eb659c21aff81
elmira1788 <elmiraalimohammadi@gmail.com>
2023-05-14 18:32:56 +1000

    second commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ const About = () => {
       <div  className='about'>
         
         <h2>Talented Software Engineer with a desire to learn and grow in a collaborative team environment.</h2>
-        <p>I am enthusiastic about using my abilities to create software solutions that positively impact people's lives. To achieve this goal, I completed the 500+ hour General Assembly Software Engineering Immersive course, which gave me a solid foundation in both front-end and back-end technologies like React, JavaScript, Node.js, and PostgreSQL.<br/>My previous experience as an accountant has honed my attention to detail, critical thinking, and methodical problem-solving skills, as well as my ability to communicate complex information clearly and concisely with clients. These abilities are highly relevant to software engineering, where effective communication is critical for successful project management and collaboration.<br/> I am committed to ongoing learning and self-improvement, and I am eager to contribute my knowledge and skills to a team working on significant projects.</p>
+        <p>I am passionate about using my abilities to develop software solutions that positively impact people's lives.<br/>My previous experience as an accountant has honed my attention to detail, critical thinking, and methodical problem-solving skills, as well as my ability to communicate complex information clearly and concisely with clients. These abilities are highly relevant to software engineering, where effective communication is critical for successful project management and collaboration.<br/> I am committed to ongoing learning and self-improvement, and I am eager to contribute my knowledge and skills to a team working on significant projects.</p>
         
 
       </div>
